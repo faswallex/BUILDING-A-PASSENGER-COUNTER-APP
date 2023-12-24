66ac69f7d7bb643d6c4a648471040a471f4c32ce
faswallex <oifasanya@gmail.com>
2023-12-25 00:14:12 +0100

    BUILDING A PASSENGER COUNTER

---
+++ index.js
@@ -0,0 +1,12 @@
+//document.getElementById("count-el").innerText = 5
+
+//let count = 0
+
+function increment() {
+    console.log("The button was clicked")
+}
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
